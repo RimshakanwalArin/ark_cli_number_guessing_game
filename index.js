7c142cc6ff8da88ef188abb0924624d51666350c
RimshakanwalArin <160338555+RimshakanwalArin@users.noreply.github.com>
2024-03-23 16:50:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "useGuessesNumber",
+        type: "number",
+        message: "Please a guessing number select 6 to 1",
+    },
+]);
+if (answer.useGuessesNumber === randomNumber) {
+    console.log("congtratulation!!! you are guess right number");
+}
+else {
+    console.log("you are guess wrong number");
+}
